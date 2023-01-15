263364364bf0794ac9c0da5e3f53c521757f1da8
mycomputer <Danil12.07.2012@gail.com>
2023-01-15 10:25:51 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача 1");
+        for(int i = 0; i < 10; i = i + 1){
+            System.out.println("Число " + i);
+        }
 
 
     }
