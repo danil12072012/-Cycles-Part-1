8e9d6bda21674db90058e19c980ff300ea2549fd
mycomputer <Danil12.07.2012@gail.com>
2023-01-15 10:56:51 +0300

    Task 8

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 7");
-        for(int i = 2; i < 513; i = i *2){
-            System.out.print(i + " ");
+        System.out.println("Задача 8");
+        int a = 0;
+
+        for(int i = 0; i < 13; i = i + 1){
+            int b = a + 29000*i;
+            System.out.println("Месяц " + i + " сумма накоплений " + b);
         }
         }
 
