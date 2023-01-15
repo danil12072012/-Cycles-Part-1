3c98d941c5da94ba42fec61d4d2a492cc4421e25
mycomputer <Danil12.07.2012@gail.com>
2023-01-15 10:36:08 +0300

    Task 5

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 4");
-        for(int i = 10; i > -10; i = i - 1){
-            System.out.println("Число " + i);
+        System.out.println("Задача 5");
+        for(int i = 1904; i < 2096; i = i +4){
+            System.out.println(i + " год является високосным.");
         }
         }
 
